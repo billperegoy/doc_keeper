569e631196e3f03fdfdde63a62dc16ee91978133
billperegoy <bill@billperegoy.com>
2016-01-02 18:51:25 -0500

    Added support for static pages. Added some sample content and styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+
   resources :sections, only: [:index, :show, :create, :new] do
     resources :sub_sections, only: [:new, :create, :index, :show]
   end
