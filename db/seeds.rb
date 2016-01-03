37f45e3ddbdc6f992514ee1fca00ea6c589676db
billperegoy <bill@billperegoy.com>
2016-01-02 19:02:44 -0500

    Added seed data and did minor view formatting cleanup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+Section.delete_all
+SubSection.delete_all
+verification = Section.create({name: "Verification"})
+SubSection.create({name: "sim_lsf", section_id: verification.id})
+SubSection.create({name: "project setup", section_id: verification.id})
+SubSection.create({name: "regression_tool", section_id: verification.id})
+
+gvp = Section.create({name: "GVP"})
+SubSection.create({name: "project setup", section_id: gvp.id})
+SubSection.create({name: "fixing hung regression", section_id: gvp.id})
+
+s2 = Section.create({name: "LSF Config"})
